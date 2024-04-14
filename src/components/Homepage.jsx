490c7d22ca2ee2e2a9eaf065203833e04dc10ef7
Ajay Kushwaha <ajkushwaha1112@gmail.com>
2024-04-14 23:22:37 +0530

    homepage and cryptocurrencies page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,13 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 
 const { Title } = Typography;
 
 export default function Homepage() {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
-  console.log("🚀 ~ Homepage ~ globalStats:", globalStats);
 
   if (isFetching) return "Loading...";
 
@@ -50,6 +50,26 @@ export default function Homepage() {
           />
         </Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Select, Typography, Avatar, Card, Row, Col } from "antd";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
-export default function News() {
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+export default function News({ simplified }) {
+  const { data, isFetching } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 10 : 100,
+  });
+  console.log("🚀 ~ News ~ count:", data);
+
   return <div>News</div>;
 }
